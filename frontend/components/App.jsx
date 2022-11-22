5de0284f4b53ed9a0ab49a3cbef002eab6103a8e
Boyi Qu <boyiqu9596@gmail.com>
2022-11-21 16:21:34 -0800

    added Chat component

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,6 +3,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import config from '../config.js'
 import io from 'socket.io-client';
+import Chat from './messages_components/Chat.jsx';
+
 const socket = io(`${config.localIP}:4000`);
 
 const styles = StyleSheet.create({
@@ -24,6 +26,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Testing with cats</Text>
+      <Chat />
       <StatusBar style="auto" />
     </View>
   );
