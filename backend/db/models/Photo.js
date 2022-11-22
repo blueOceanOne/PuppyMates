3f61a63fd3865c9cdae7af3907a2636beb70491b
KateFeaster <zencornhopper@gmail.com>
2022-11-21 17:34:31 -0800

    Added Photo Model

---
+++ backend/db/models/Photo.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require('sequelize');
+const db = require('../db');
+const User = require('./User');
+
+const Photo = db.define('photo', {
+  user_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: User,
+      key: 'id',
+    },
+    allowNull: false,
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+});
+
+Photo.sync();
+
+module.exports = Photo;
