3a23b75444cee424843647b49b87adeb513cac79
KateFeaster <zencornhopper@gmail.com>
2022-11-21 17:34:48 -0800

    Added User Model

---
+++ backend/db/models/User.js
@@ -0,0 +1,59 @@
+const { DataTypes } = require('sequelize');
+const db = require('../db');
+const Breed = require('./Breed');
+
+const User = db.define('user', {
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  dog_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  breed_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Breed,
+      key: 'id',
+    },
+    allowNull: false,
+  },
+  size: {
+    type: DataTypes.ENUM('extra small', 'small', 'medium', 'large', 'extra large'),
+    allowNull: false,
+  },
+  dog_friendly: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+  people_friendly: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+  energy: {
+    type: DataTypes.ENUM('low', 'average', 'high'),
+    allowNull: false,
+  },
+  city: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  state: {
+    type: DataTypes.STRING(2),
+    allowNull: false,
+  },
+  latitude: {
+    type: DataTypes.DOUBLE,
+  },
+  longitude: {
+    type: DataTypes.DOUBLE,
+  },
+  bio: {
+    type: DataTypes.STRING(1000),
+  },
+});
+
+User.sync();
+
+module.exports = User;
