6b38f66727094a7cf4de244b1fd41f1db600c35f
KateFeaster <zencornhopper@gmail.com>
2022-11-21 17:34:12 -0800

    Added invitation model

---
+++ backend/db/models/Invitation.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require('sequelize');
+const db = require('../db');
+const User = require('./User');
+const Event = require('./Event');
+
+const Invitation = db.define('invitation', {
+  invitee_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: User,
+      key: 'id',
+    },
+    allowNull: false,
+  },
+  event_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Event,
+      key: 'id',
+    },
+    allowNull: false,
+  },
+  status: {
+    type: DataTypes.ENUM('sent', 'accepted', 'declined'),
+    allowNull: false,
+  },
+});
+
+Invitation.sync();
+
+module.exports = Invitation;
