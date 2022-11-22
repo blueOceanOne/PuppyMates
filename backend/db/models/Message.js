c3311eaee39260c7cf5eb7db956a29551c25be7d
KateFeaster <zencornhopper@gmail.com>
2022-11-21 17:34:18 -0800

    Added Message model

---
+++ backend/db/models/Message.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require('sequelize');
+const db = require('../db');
+const User = require('./User');
+
+const Message = db.define('message', {
+  sender_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: User,
+      key: 'id',
+    },
+    allowNull: false,
+  },
+  recipient_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: User,
+      key: 'id',
+    },
+    allowNull: false,
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+});
+
+Message.sync();
+
+module.exports = Message;
