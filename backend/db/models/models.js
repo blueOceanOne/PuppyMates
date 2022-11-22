73593ea78172bd08ad04a3fe81a1fdbb7af4fe5e
KateFeaster <zencornhopper@gmail.com>
2022-11-21 17:34:57 -0800

    Added associations and collected models into one object

---
+++ backend/db/models/models.js
@@ -0,0 +1,93 @@
+const Breed = require('./Breed');
+const Event = require('./Event');
+const Invitation = require('./Invitation');
+const Message = require('./Message');
+const Photo = require('./Photo');
+const Request = require('./Request');
+const User = require('./User');
+
+Breed.hasMany(User, {
+  foreignKey: {
+    name: 'breed_id',
+    allowNull: false,
+  },
+});
+User.belongsTo(Breed);
+
+User.hasMany(Photo, {
+  foreignKey: {
+    name: 'user_id',
+    allowNull: false,
+  },
+});
+Photo.belongsTo(User);
+
+User.hasMany(Message, {
+  foreignKey: {
+    name: 'sender_id',
+    allowNull: false,
+  },
+  as: 'sender',
+});
+Message.belongsTo(User, {
+  as: 'sender',
+});
+
+User.hasMany(Message, {
+  foreignKey: {
+    name: 'recipient_id',
+    allowNull: false,
+  },
+  as: 'recipient',
+});
+Message.belongsTo(User, {
+  as: 'recipient',
+});
+
+User.hasMany(Request, {
+  foreignKey: {
+    name: 'sender_id',
+    allowNull: false,
+  },
+  as: 'sender',
+});
+Request.belongsTo(User, {
+  as: 'sender',
+});
+
+User.hasMany(Request, {
+  foreignKey: {
+    name: 'recipient_id',
+    allowNull: false,
+  },
+  as: 'recipient',
+});
+Request.belongsTo(User, {
+  as: 'recipient',
+});
+
+User.hasMany(Invitation, {
+  foreignKey: {
+    name: 'invitee_id',
+    allowNull: false,
+  },
+});
+Invitation.belongsTo(User);
+
+User.hasMany(Event, {
+  foreignKey: {
+    name: 'host_id',
+    allowNull: false,
+  },
+});
+Invitation.belongsTo(User);
+
+Event.hasMany(Invitation, {
+  foreignKey: {
+    name: 'event_id',
+    allowNull: false,
+  },
+});
+Invitation.belongsTo(Event);
+
+module.exports = { Breed, Event, Invitation, Message, Photo, Request, User };
