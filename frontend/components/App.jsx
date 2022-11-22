c996b07cb6497ff117672478ed4733f46cc4a612
Boyi Qu <boyiqu9596@gmail.com>
2022-11-21 22:46:20 -0800

    imported main tab components and successfully imported createBottomTabNavigator

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import config from '../config.js'
 import io from 'socket.io-client';
 import Chat from './messages_components/Chat.jsx';
-
+import Home from './Home.jsx';
+import Events from './Events.jsx';
+import Profile from './Profile.jsx';
 const socket = io(`${config.localIP}:4000`);
 
 const styles = StyleSheet.create({
@@ -26,7 +29,6 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Testing with cats</Text>
-      <Chat />
       <StatusBar style="auto" />
     </View>
   );
