442cde7d8efde4273b39d9bfa72fd08619198cf8
Boyi Qu <boyiqu9596@gmail.com>
2022-11-23 11:12:00 -0800

    rendered pending requests

--- frontend/components/messages_components/Requests.jsx
+++ frontend/components/messages_components/Requests.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { View, Text } from 'react-native'
+import { View, Text } from 'react-native';
+import { ListItem, Avatar } from '@rneui/themed';
+import pendingData from './requestsData.js';
 
 const Requests = () => {
   return (
     <View>
-      <Text>Requests</Text>
+      <Text>Pending requests</Text>
+      {
+        pendingData.map((item, i) => (
+          <ListItem key={i} bottomDivider>
+            <Avatar source={{uri: item.photo}} />
+            <ListItem.Content>
+              <ListItem.Title>{item.dog_name}</ListItem.Title>
+            </ListItem.Content>
+          </ListItem>
+        ))
+      }
     </View>
   )
 }
