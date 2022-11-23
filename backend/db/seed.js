1ff1b5475f7796de3c042cd00add9c4423efc63c
KateFeaster <zencornhopper@gmail.com>
2022-11-22 16:21:52 -0800

    Got started on building seeding

--- backend/db/seed.js
+++ backend/db/seed.js
@@ -1,1 +1,9 @@
-const db = require('./db.js');
+const path = require('path');
+const db = require('./db');
+
+db.query(`
+COPY breeds(id, breed, createdAt, updatedAt)
+FROM '${path.join(__dirname, './sampleData/example_breeds.csv')}'
+DELIMITER ','
+CSV HEADER;
+`);
