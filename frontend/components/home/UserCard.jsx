8feed1c7d967a0fac1d56b6f746946418a67994f
Kally Cao <kallycao98@gmail.com>
2022-11-22 20:21:01 -0800

    Implemented default and expanded dog card displays, swipe functionality WIP

---
+++ frontend/components/home/UserCard.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { View, Image, Dimensions, Animated, PanResponder } from 'react-native';
+import { Text, Card, Button, Icon, Divider } from '@rneui/themed';
+const { useState, useEffect, useRef } = React;
+import userData from '../home/exampleData/userData.js';
+import MoreInfo from '../home/MoreInfo.jsx';
+
+const dWidth = Dimensions.get('window').width;
+const dHeight = Dimensions.get('window').height;
+
+const UserCard = ({ item, index, onSwipe }) => {
+  const [viewMore, setViewMore] = useState(false);
+
+  return (
+    <View>
+      <Card>
+        <Image
+          source={{
+            uri: item.photos[0],
+          }}
+          style={{ height: dHeight * 0.6, width: dWidth * 0.9 }}
+          alignSelf="center"
+          resizeMode="cover"
+        />
+        <View flexDirection="row" justifyContent="space-between">
+          <Text h3>{item.dog_name}</Text>
+          <Text h3># miles away</Text>
+        </View>
+        <Button title="Clear" type="clear" onPress={() => setViewMore(!viewMore)}>
+          {viewMore ? 'View Less' : 'View More'}
+        </Button>
+        {viewMore ? <MoreInfo item={item} /> : null}
+      </Card>
+    </View>
+  );
+};
+
+export default UserCard;
