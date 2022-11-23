ac793abe89f32618f12d8bb2c9d160e4c0ac9e41
Boyi Qu <boyiqu9596@gmail.com>
2022-11-23 13:23:30 -0800

    rendered chat list of matched users and clicking on chat navigates to chat page

--- frontend/components/messages_components/ChatsList.jsx
+++ frontend/components/messages_components/ChatsList.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text } from 'react-native'
 import ChatPage from './ChatPage.jsx';
 import { Button } from '@rneui/themed';
 import { useNavigation } from '@react-navigation/native';
+import { ListItem, Avatar } from '@rneui/themed';
+import matchedData from './matchedData.js';
 
 const ChatsList = ({socket}) => {
   const navigation = useNavigation();
@@ -10,12 +12,24 @@ const ChatsList = ({socket}) => {
   return (
     <View>
       <Text>ChatsList</Text>
-      <Button
-        title="Chat in Room 1"
-        onPress = {(event)=>{
-        event.preventDefault();
-        navigation.navigate('ChatPage')
-      }} />
+      {
+        matchedData.map((item, i)=>(
+          <ListItem
+            key={i}
+            bottomDivider
+            onPress = {(event)=>{
+              event.preventDefault();
+              navigation.navigate('ChatPage')
+            }}
+          >
+            <Avatar source={{uri: item.photo}} />
+            <ListItem.Content>
+              <ListItem.Title>{item.dog_name}</ListItem.Title>
+              <ListItem.Subtitle style={{color: "grey"}}>Start chatting</ListItem.Subtitle>
+            </ListItem.Content>
+          </ListItem>
+        ))
+      }
     </View>
   )
 }
--- frontend/components/messages_components/matchedData.js
+++ frontend/components/messages_components/matchedData.js
@@ -3,8 +3,9 @@ const matchedData = [
     id: 1,
     user1_id: 1,
     user2_id: 4,
+    dog_name: 'Golden',
     status: 'accepted',
-    photo: 'https://images.unsplash.com/photo-1546975490-a79abdd54533?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y29yZ2l8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60'
+    photo: 'https://images.unsplash.com/photo-1552053831-71594a27632d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=662&q=80'
   },
 
   {
@@ -12,6 +13,9 @@ const matchedData = [
     user1_id: 1,
     user2_id: 5,
     status: 'accepted',
-    photo: 'https://images.unsplash.com/photo-1649923625793-af1cbd33c8bd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXxYZDhRNzZxRTNEZ3x8ZW58MHx8fHw%3D&auto=format&fit=crop&w=700&q=60'
+    dog_name: 'Lily',
+    photo: 'https://images.unsplash.com/photo-1560525821-d5615ef80c69?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fGRvZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60'
   },
 ]
+
+export default matchedData;
