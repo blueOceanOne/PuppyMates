51360edc2106a78a28959c468bdb32cb3f05f70a
KateFeaster <zencornhopper@gmail.com>
2022-11-22 19:19:07 -0800

    Added null condition

--- backend/db/seed.js
+++ backend/db/seed.js
@@ -13,6 +13,7 @@ CSV HEADER;
 COPY users("id", "email", "dog_name", "breed_id", "size", "dog_friendly", "people_friendly", "energy", "city", "state", "bio", "createdAt", "updatedAt")
 FROM '${path.join(__dirname, './sampleData/example_users.csv')}'
 DELIMITER ','
+NULL AS 'null'
 CSV HEADER;
 `);
 });
