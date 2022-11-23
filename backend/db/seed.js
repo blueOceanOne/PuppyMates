a8b17f5d533466da9fbf59a56f6955cda30f0034
KateFeaster <zencornhopper@gmail.com>
2022-11-22 20:33:25 -0800

    Added copies for photos and events

--- backend/db/seed.js
+++ backend/db/seed.js
@@ -15,12 +15,26 @@ TRUNCATE TABLE breeds CASCADE;`
   `
     )
   )
-  .then(() => {
+  .then(() =>
     db.query(`
 COPY users("id", "email", "dog_name", "breed_id", "size", "dog_friendly", "people_friendly", "energy", "city", "state", "bio", "createdAt", "updatedAt")
 FROM '${path.join(__dirname, './sampleData/example_users.csv')}'
 DELIMITER ','
 NULL AS 'null'
 CSV HEADER;
-`);
-  });
+`)
+  )
+  .then(() =>
+    db.query(`COPY photos("id","user_id","url","createdAt","updatedAt")
+  FROM '${path.join(__dirname, './sampleData/example_photos.csv')}'
+  DELIMITER ','
+  CSV HEADER;`)
+  )
+  .then(() =>
+    db.query(
+      `COPY events("id", "host_id", "title", "description", "date", "latitude", "longitude", "createdAt", "updatedAt")
+      FROM '${path.join(__dirname, './sampleData/example_events.csv')}'
+      DELIMITER ','
+      CSV HEADER`
+    )
+  );
