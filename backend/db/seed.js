04bc000319e9441b5e027ba7b3c3d19436e25b5c
KateFeaster <zencornhopper@gmail.com>
2022-11-22 17:39:38 -0800

    Added step for seeding users

--- backend/db/seed.js
+++ backend/db/seed.js
@@ -1,9 +1,18 @@
 const path = require('path');
 const db = require('./db');
 
-db.query(`
-COPY breeds(id, breed, createdAt, updatedAt)
+db.query(
+  `
+COPY breeds(id, breed, "createdAt", "updatedAt")
 FROM '${path.join(__dirname, './sampleData/example_breeds.csv')}'
 DELIMITER ','
 CSV HEADER;
+`
+).then(() => {
+  db.query(`
+COPY users("id", "email", "dog_name", "breed_id", "size", "dog_friendly", "people_friendly", "energy", "city", "state", "bio", "createdAt", "updatedAt")
+FROM '${path.join(__dirname, './sampleData/example_users.csv')}'
+DELIMITER ','
+CSV HEADER;
 `);
+});
