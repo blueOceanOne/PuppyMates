35872486fe8c7bedbab1fa9e4bcb11e58419ffb4
Taylor <taylor.sheets3@gmail.com>
2022-11-23 11:43:13 -0800

    added controller functions for all endpoints except swipe and updateUser

--- backend/server/controllers.js
+++ backend/server/controllers.js
@@ -1,5 +1,5 @@
 require('../db/models/models');
-
+const sequelize = require('sequelize')
 const Breed = require('../db/models/Breed');
 const Event = require('../db/models/Event');
 const Invitation = require('../db/models/Invitation');
@@ -8,85 +8,158 @@ const Photo = require('../db/models/Photo');
 const Request = require('../db/models/Request');
 const User = require('../db/models/User');
 
+const { Op } = sequelize;
 
 module.exports = {
-  getUser: function(req, res) {
+  getUser: function (req, res) {
     let user_id = req.url.slice(7);
     User.findAll({
       where: {
-        id: user_id
-      }
-    })
-      .then((result) => {
-        console.log(result)
-        res.send(result);
+        id: user_id,
+      },
+    }).then((result) => {
+      res.send(result);
+    });
+  },
+
+  postUser: function (req, res) {
+    let user = User.create(req.body)
+      .then((user) => {
+        res.send(user);
       });
   },
 
-  postUser: function(req, res) {
-    // let user = User.create(req.body)
-    //   .then(() => res.send(user));
-  },
-
-  updateUser: function(req, res) {
+  updateUser: function (req, res) {
+    //TODO
     res.send('received');
   },
 
-  swipe: function(req, res) {
+  swipe: function (req, res) {
+    //TODO
     //determine left or right swipe from req.url
     //body includes sender and recipient ids
     res.send('received');
   },
 
-  getMessages: function(req, res) {
-    //participant_id is query param, user_id is req.url
-    res.send('received');
+  getMessages: function (req, res) {
+    let user = req._parsedUrl.pathname.slice(10);
+    let sender = req.query.participant_id;
+
+    Message.findAll({
+      where: {
+        [Op.or]: [
+        {
+          sender_id: user,
+          recipient_id: sender
+        },
+        {
+          recipient_id: user,
+          sender_id: sender
+        }
+        ]
+      },
+      order: [['createdAt', 'DESC']]
+    })
+    .then((result) => res.send(result));
   },
 
-  postMessages: function(req, res) {
-    //body includes sender/recipient ids and content
-    //console.log(req.body);
+  postMessages: function (req, res) {
     const message = Message.create(req.body)
+      .then((message) => res.send(message));
+  },
+
+  getPendingRequests: function (req, res) {
+    let user = req.url.slice(18);
+
+    Request.findAll({
+      where: {
+        recipient_id: user,
+        status: 'pending'
+      }
+    }).then((result) => res.send(result));
+  },
+
+  getAcceptedRequests: function (req, res) {
+    let user = req.url.slice(19);
+
+    Request.findAll({
+      where: {
+        recipient_id: user,
+        status: 'accepted'
+      }
+    }).then((result) => res.send(result));
+  },
+
+  acceptRequest: function (req, res) {
+    let user = req._parsedUrl.pathname.slice(17);
+    let sender = req.query.participant_id;
+    Request.update({status: 'accepted'}, {where: {recipient_id: user, sender_id: sender}})
+      .then(() => res.send('received'));
+  },
+
+  rejectRequest: function (req, res) {
+    let user = req._parsedUrl.pathname.slice(17);
+    let sender = req.query.participant_id;
+    Request.update({status: 'rejected'}, {where: {recipient_id: user, sender_id: sender}})
+      .then(() => res.send('received'));
+  },
+
+  getAcceptedEvents: function (req, res) {
+    let user = req.url.slice(23)
+    Invitation.findAll({
+      where: {
+        invitee_id: user,
+        status: 'accepted'
+      },
+      include: [{
+        model: Event
+      }]
+    })
+    .then((result) => res.send(result));
+  },
+
+  getPendingEvents: function (req, res) {
+    let user = req.url.slice(21)
+    Invitation.findAll({
+      where: {
+        invitee_id: user,
+        status: 'pending'
+      },
+      include: [{
+        model: Event
+      }]
+    })
+    .then((result) => res.send(result));
+  },
+
+  postEvent: function (req, res) {
+    const event = Event.create(req.body)
       .then((result) => res.send(result));
   },
 
-  getPendingRequests: function(req, res) {
-    console.log(req.query.participant_id);
-    res.send('received');
+  acceptEvent: function (req, res) {
+    let user = req._parsedUrl.pathname.slice(21);
+    let event = req.query.event_id;
+
+    Invitation.update({status: 'accepted'}, {
+      where: {
+        invitee_id: user,
+        event_id: event
+      }
+    })
+    .then((result) => res.send(result));
   },
 
-  getAcceptedRequests: function(req, res) {
-    res.send('received');
-  },
+  rejectEvent: function (req, res) {
+    let user = req._parsedUrl.pathname.slice(20);
+    let event = req.query.event_id;
 
-  acceptRequest: function(req, res) {
-    res.send('received');
+    Invitation.update({status: 'declined'}, {
+      where: {
+        invitee_id: user,
+        event_id: event
+      }
+    })
+    .then((result) => res.send(result));
   },
-
-  rejectRequest: function(req, res) {
-    res.send('received');
-  },
-
-  getAcceptedEvents: function(req, res) {
-    res.send('received');
-  },
-
-  getPendingEvents: function(req, res) {
-    res.send('received');
-  },
-
-  postEvent: function(req, res) {
-    //body includes host id, date, title, description, latitude, longitude
-    res.send('received');
-  },
-
-  acceptEvent: function(req, res) {
-    //event_id is a query param
-    res.send('received');
-  },
-
-  rejectEvent: function(req, res) {
-    //event_id is a query param
-    res.send('received');
-  },
-}
+};
