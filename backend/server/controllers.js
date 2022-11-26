57b8eaff0334b8f99ec41fcb4924df720d4bbcb1
KateFeaster <zencornhopper@gmail.com>
2022-11-25 17:27:40 -0800

    Sorted getAcceptedEvents

--- backend/server/controllers.js
+++ backend/server/controllers.js
@@ -240,6 +240,7 @@ module.exports = {
           ],
         },
       ],
+      order: [[Event, 'date', 'DESC']],
     })
       .then((result) => res.status(200).json(result))
       .catch((err) => {
