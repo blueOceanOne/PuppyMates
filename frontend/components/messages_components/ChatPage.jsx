f634da51ccdfae2f6b4d6388fb476ec716242245
Boyi Qu <boyiqu9596@gmail.com>
2022-11-25 16:44:21 -0800

    Revert "Revert "getting database data in messages""
    This reverts commit db329bf5ced6d610b4b97738b24c80d27da198b7.

--- frontend/components/messages_components/ChatPage.jsx
+++ frontend/components/messages_components/ChatPage.jsx
@@ -2,19 +2,22 @@ import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 import ChatBody from './ChatBody.jsx';
 import ChatInput from './ChatInput.jsx';
+import config from '../../config.js';
+import axios from 'axios';
 
 const ChatPage = ({socket, selectedRecipient, user}) => {
 
   const [messages, setMessages] = useState([]);
   const [recipient, setRecipient] = useState(selectedRecipient)
-
-/*   useEffect(()=>{
-    socket.emit('requestID', user);
-    socket.on('sendID', (newUser)=>{
-      console.log('socketid is ', newUser);
-      setUser(newUser.id);
-    })
-  }, []) */
+  useEffect(() => {
+    axios.get(`http://${config.localIP}:${config.port}/messages/${user}`, { params: {participant_id: selectedRecipient}})
+      .then((response)=>{
+        setMessages(response.data);
+      })
+      .catch((err)=>{
+        console.log(err);
+      })
+  })
 
   useEffect(() => {
     socket.on('response', (data) => {
