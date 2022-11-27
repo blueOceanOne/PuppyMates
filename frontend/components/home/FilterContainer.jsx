350c4e2b802f1fca9206fb6910f4f13f0b34931e
Kally Cao <kallycao98@gmail.com>
2022-11-27 09:15:44 -0800

    Save current changes to merge new changes from main branch

--- frontend/components/home/FilterContainer.jsx
+++ frontend/components/home/FilterContainer.jsx
@@ -9,18 +9,92 @@ const breeds = userData.map((user) => user.breed);
 const sizes = ['Small', 'Medium', 'Large'];
 const energy = ['Low', 'Medium', 'High'];
 
-const FilterContainer = ({ filter, setFilter }) => {
+const FilterContainer = ({ filter, setFilter, handleFilter }) => {
   const [expandSize, setExpandSize] = useState(false);
   const [expandEnergy, setExpandEnergy] = useState(false);
   const [expandBreed, setExpandBreed] = useState(false);
   const [expandFilter, setExpandFilter] = useState(false);
   const [displayConfirm, setDisplayConfirm] = useState(false);
   const [selection, setSelection] = useState({});
+  const [value, setValue] = useState('');
   const [category, setCategory] = useState('');
+
+  const handleExpandFilter = () => {
+    if (expandFilter) {
+      setValue('');
+      setCategory('');
+      setExpandFilter(false);
+    } else {
+      setExpandFilter(true);
+    }
+  };
+
+  const handleExpandBreed = () => {
+    if (expandBreed) {
+      setCategory('');
+      setExpandBreed(false);
+    } else {
+      setExpandBreed(true);
+      setCategory('breed');
+    }
+  };
+
+  const handleExpandSize = () => {
+    if (expandSize) {
+      setCategory('');
+      setExpandSize(false);
+    } else {
+      setExpandSize(true);
+      setCategory('size');
+    }
+  };
+  const handleExpandEnergy = () => {
+    if (expandEnergy) {
+      setCategory('');
+      setExpandEnergy(false);
+    } else {
+      setExpandEnergy(true);
+      setCategory('energy');
+    }
+  };
 
   const handleSelection = (string) => {
     setDisplayConfirm(true);
     setSelection({ category: category, value: string });
+    console.log(selection);
+  };
+
+  const handleValue = (currVal) => {
+    if (value === currVal) {
+      setValue('');
+      setDisplayConfirm(false);
+    } else {
+      setValue(currVal);
+      setDisplayConfirm(true);
+    }
+  };
+
+  const handleDogFriendly = () => {
+    if (category === 'Dog Friendly') {
+      setCategory('');
+      setValue('');
+      setDisplayConfirm(false);
+    } else {
+      setCategory('Dog Friendly');
+      setValue('dog_friendly');
+      setDisplayConfirm(true);
+    }
+  };
+  const handlePeopleFriendly = () => {
+    if (category === 'People Friendly') {
+      setCategory('');
+      setValue('');
+      setDisplayConfirm(false);
+    } else {
+      setCategory('People Friendly');
+      setValue('people_friendly');
+      setDisplayConfirm(true);
+    }
   };
 
   const handleConfirm = () => {
@@ -46,9 +120,7 @@ const FilterContainer = ({ filter, setFilter }) => {
           </>
         }
         isExpanded={expandFilter}
-        onPress={() => {
-          setExpandFilter(!expandFilter);
-        }}
+        onPress={() => handleExpandFilter()}
       >
         <ListItem.Accordion
           content={
@@ -58,19 +130,16 @@ const FilterContainer = ({ filter, setFilter }) => {
               </ListItem.Content>
             </>
           }
-          isExpanded={expandBreed}
-          onPress={() => {
-            setExpandBreed(!expandBreed);
-            setCategory('Breed');
-          }}
+          isExpanded={category === 'breed' ? true : false}
+          onPress={() => handleExpandBreed()}
           bottomDivider
         >
           {breeds.map((breed) => (
-            <ListItem key={breed} onPress={() => handleSelection(breed)} bottomDivider>
+            <ListItem key={breed} onPress={() => handleValue(breed)} bottomDivider>
               <ListItem.Content>
                 <ListItem.Title>
                   {breed}
-                  {breed === selection.value ? <Icon name="check" /> : null}
+                  {breed === value ? <Icon name="check" /> : null}
                 </ListItem.Title>
               </ListItem.Content>
             </ListItem>
@@ -84,19 +153,16 @@ const FilterContainer = ({ filter, setFilter }) => {
               </ListItem.Content>
             </>
           }
-          isExpanded={expandSize}
-          onPress={() => {
-            setExpandSize(!expandSize);
-            setCategory('Size');
-          }}
+          isExpanded={category === 'size' ? true : false}
+          onPress={() => handleExpandSize()}
           bottomDivider
         >
           {sizes.map((size) => (
-            <ListItem key={size} onPress={() => handleSelection(size)} bottomDivider>
+            <ListItem key={size} onPress={() => handleValue(size)} bottomDivider>
               <ListItem.Content>
                 <ListItem.Title>
                   {size}
-                  {size === selection.value ? <Icon name="check" /> : null}
+                  {size === value ? <Icon name="check" /> : null}
                 </ListItem.Title>
               </ListItem.Content>
             </ListItem>
@@ -110,50 +176,35 @@ const FilterContainer = ({ filter, setFilter }) => {
               </ListItem.Content>
             </>
           }
-          isExpanded={expandEnergy}
-          onPress={() => {
-            setExpandEnergy(!expandEnergy);
-            setCategory('Energy');
-          }}
+          isExpanded={category === 'energy' ? true : false}
+          onPress={() => handleExpandEnergy()}
           bottomDivider
         >
           {energy.map((level) => (
-            <ListItem key={level} onPress={() => handleSelection(level)} bottomDivider>
+            <ListItem key={level} onPress={() => handleValue(level)} bottomDivider>
               <ListItem.Content>
                 <ListItem.Title>
                   {level}
-                  {level === selection.value ? <Icon name="check" /> : null}
+                  {level === value ? <Icon name="check" /> : null}
                 </ListItem.Title>
               </ListItem.Content>
             </ListItem>
           ))}
         </ListItem.Accordion>
 
-        <ListItem
-          onPress={() => {
-            setCategory('Dog Friendly');
-            handleSelection('Dog Friendly');
-          }}
-          bottomDivider
-        >
+        <ListItem onPress={() => handleDogFriendly()} bottomDivider>
           <ListItem.Content>
             <ListItem.Title style={{ fontWeight: '600' }}>
               Dog Friendly
-              {/* {'Dog Friendly' === selection.value ? <Icon name="check" /> : null} */}
+              {'Dog Friendly' === category ? <Icon name="check" /> : null}
             </ListItem.Title>
           </ListItem.Content>
         </ListItem>
-        <ListItem
-          onPress={() => {
-            setCategory('People Friendly');
-            handleSelection('People Friendly');
-          }}
-          bottomDivider
-        >
+        <ListItem onPress={() => handlePeopleFriendly()} bottomDivider>
           <ListItem.Content>
             <ListItem.Title style={{ fontWeight: '600' }}>
               People Friendly
-              {/* {'People Friendly' === selection.value ? <Icon name="check" /> : null} */}
+              {'People Friendly' === category ? <Icon name="check" /> : null}
             </ListItem.Title>
           </ListItem.Content>
         </ListItem>
--- frontend/components/home/Home.jsx
+++ frontend/components/home/Home.jsx
@@ -1,23 +1,82 @@
 import React from 'react';
 import { View, Text, SafeAreaView } from 'react-native';
-const { useState } = React;
+import axios from 'axios';
 import FilterContainer from './FilterContainer.jsx';
 import CarouselCards from './CarouselCards.jsx';
 //prop to be passed in to get current user's id
+const { useState, useEffect } = React;
 const Home = () => {
-  const [filter, setFilter] = useState({});
+  // const Home = ({id}) => {
+  const initialFilter = { filterCategory: '', filterValue: '' };
+  const [filter, setFilter] = useState(initialFilter);
   const [localUsers, setLocalUsers] = useState([]);
 
-  //useEffect to render users on load and set to setLocalUsers state
+  //SINGLE USERS
+  // axios.get(`http://${config.localIP}:${config.port}/users`);
 
-  //create handleFilter func to send get request
+  //LOCAL USERS
+  // useEffect(() => {
+  //   axios.get(`http://${config.localIP}:${config.port}/home?id=${id}`)
+  //     .then((data) => setLocalUsers(data))
+  //     .catch((err) => res.send(err))
+  // }, []);
+
+  //HANDLE FILTER FUNCTION
+  const handleFilter = () => {
+    console.log('handle filter');
+    //   axios
+    //     .get(
+    //       `http://${config.localIP}:${config.port}/home?id=${id}&filterCategory=${filter.filterCategory}&filterValue=${filter.filterValue}`
+    //     )
+    //     .then((data) => {
+    //       setLocalUsers(data);
+    //       setFilter(initialFilter);
+    //     })
+    //     .catch((err) => res.send(err));
+  };
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <FilterContainer filter={filter} setFilter={setFilter} />
+      <FilterContainer filter={filter} setFilter={setFilter} handleFilter={handleFilter} />
       <CarouselCards localUsers={localUsers} />
     </SafeAreaView>
   );
 };
 
 export default Home;
+
+// const filters = [
+//   {
+//   category: 'breed',
+//   badge: 'Breed',
+//   values: [breeds],
+//   isExpanded: 'expandBreed',
+//   onPress: 'setExpandBreed(!expandBreed)'
+//   },
+//   {
+//     category: 'size',
+//     badge: 'Size',
+//     values: ['Small', 'Medium', 'Large'],
+//     isExpanded: 'expandSize',
+//     onPress: 'setExpandSize(!expandSize)'
+//   },
+//   {
+//     category: 'energy',
+//     badge: 'Energy',
+//     values: ['Low', 'Medium', 'High'],
+//     isExpanded: 'expandEnergy',
+//     onPress: 'setExpandEnergy(!expandEnergy)'
+//   },
+//   {
+//     category: 'dog_friendly',
+//     badge: 'Dog Friendly',
+//     values: 'Dog Friendly',
+//     onPress: 'setExpandBreed(!expandBreed)'
+//   },
+//   {
+//     category: 'people_friendly',
+//     badge: 'People Friendly',
+//     values: 'People Friendly'
+
+//   }
+// ]
