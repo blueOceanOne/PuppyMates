af83aa816f4288ff7dca1e6c8b8807f0b11f0eba
Boyi Qu <boyiqu9596@gmail.com>
2022-11-27 12:47:58 -0800

    implemented Reject functionality

--- frontend/components/messages_components/RequestDetail.jsx
+++ frontend/components/messages_components/RequestDetail.jsx
@@ -7,11 +7,9 @@ import axios from 'axios';
 const RequestDetail = ({selectedRequest, user, setMatched, setPending}) => {
   const [selectedUser, setSelectedUser] = useState('empty');
   useEffect (()=>{
-    axios.get(`http://${config.localIP}:${config.port}/users`, {
-      params: {user_id: selectedRequest}
-    })
+    axios.get(`http://${config.localIP}:${config.port}/users/${selectedRequest}`)
     .then((response)=>{
-      setSelectedUser(response.data[0])
+      setSelectedUser(response.data)
     })
     .catch((err)=>{
       console.log(err);
@@ -20,11 +18,11 @@ const RequestDetail = ({selectedRequest, user, setMatched, setPending}) => {
 
   const navigation = useNavigation();
 
-  const handleAccept = () => {
-    axios.put(`http://${config.localIP}:${config.port}/requests/accept/${user}`, null, { params: { participant_id: selectedRequest}})
+  const handleAcceptorReject = (action) => {
+    axios.put(`http://${config.localIP}:${config.port}/requests/${action}/${user}`, null, { params: { participant_id: selectedRequest}})
     .then(()=>{
       console.log('accepted');
-      let promise1 = axios.get(`http://${config.localIP}:${config.port}/requests/accepted/${user}`);
+      let promise1 = axios.get(`http://${config.localIP}:${config.port}/requests/matched/${user}`);
       let promise2 = axios.get(`http://${config.localIP}:${config.port}/requests/pending/${user}`)
       Promise.all([promise1, promise2])
         .then((values) => {
@@ -50,14 +48,15 @@ const RequestDetail = ({selectedRequest, user, setMatched, setPending}) => {
           title="Accept"
           onPress = {(event)=>{
             event.preventDefault();
-            handleAccept();
+            handleAcceptorReject('accept');
           }}
           >Accept</Button>
         <Button
           title="Reject"
           onPress={(event)=>{
             event.preventDefault();
-/*             handleReject() */}}
+            handleAcceptorReject('reject');
+          }}
           >Reject</Button>
       </View>
     )
