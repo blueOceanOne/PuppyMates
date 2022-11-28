fe90726d35d6da49feaa0cb2fd7b492b6c1ef4b3
avaboonzaayer <sam.boonzaayer@gmail.com>
2022-11-28 11:05:42 -0700

    hardcoded profile data

--- frontend/components/profile/ViewProfile.jsx
+++ frontend/components/profile/ViewProfile.jsx
@@ -4,23 +4,6 @@ import { View, Text, Button, Image, StyleSheet, Dimensions } from 'react-native'
 export default ViewProfile = ({ navigation }) => {
   const dWidth = Dimensions.get('window').width;
   const dHeight = Dimensions.get('window').height;
-  const selectedUser = {
-    "bio": "Bio ",
-    "breed": "DACHSUND",
-    "city": "Peoria",
-    "dog_friendly": true,
-    "dog_name": "Lucy",
-    "user_email": "avarice.boonzaayer@gmail.coma",
-    "energy": "low",
-    "password": "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4",
-    "people_friendly": true,
-    "photos": [
-      "file:///data/user/0/host.exp.exponent/cache/ExperienceData/%2540anonymous%252Ffrontend-52647eb6-932d-415c-b6f5-a7e52f5f9912/ImagePicker/73c041e6-f3af-4236-9a5b-3eabcfe1a428.jpeg"
-    ],
-    "size": "small",
-    "state": "AZ",
-    "under18": false
-  }
 
   return (
     <View style={styles.moreInfo}>
@@ -39,17 +22,17 @@ export default ViewProfile = ({ navigation }) => {
         margin: 10,
         borderRadius: 20
       }}
-      source={selectedUser.photos[0]} />
+      source={'https://www.akc.org/wp-content/uploads/2017/11/Longhaired-Dachshund-standing-outdoors.jpg'} />
       <Text style={{
         alignSelf: 'center'
-      }}>{selectedUser.dog_name}</Text>
+      }}>Lucy</Text>
       <View>
         <Text style={{ fontSize: 20, fontWeight: 'bold', paddingLeft: 5 }}>
-          {`${selectedUser.city}, ${selectedUser.state}`}
+          Peoria, AZ
         </Text>
         <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
           <Chip
-            title={selectedUser.breed[0] + selectedUser.breed.slice(1).toLowerCase()}
+            title="Dachsund"
             size="xs"
             color="#FFD8A9"
             containerStyle={{ padding: 2 }}
@@ -57,7 +40,7 @@ export default ViewProfile = ({ navigation }) => {
           />
 
           <Chip
-            title={selectedUser.size[0].toUpperCase() + selectedUser.size.slice(1)}
+            title="Small"
             size="xs"
             color="#FFD8A9"
             containerStyle={{ padding: 2 }}
@@ -65,34 +48,22 @@ export default ViewProfile = ({ navigation }) => {
           />
 
           <Chip
-            title={`${selectedUser.energy[0].toUpperCase() + selectedUser.energy.slice(1)} Energy`}
+            title="Low Energy"
             size="xs"
             color="#FFD8A9"
             containerStyle={{ padding: 2 }}
             titleStyle={{ color: 'black' }}
           />
 
-          {selectedUser.people_friendly ? (
-            <Chip
-              title="People Friendly"
-              size="xs"
-              color="#FFD8A9"
-              containerStyle={{ padding: 2 }}
-              titleStyle={{ color: 'black' }}
-            />
-          ) : null}
-
-          {selectedUser.dog_friendly ? (
-            <Chip
-              title="Dog Friendly"
-              size="xs"
-              color="#FFD8A9"
-              containerStyle={{ padding: 2 }}
-              titleStyle={{ color: 'black' }}
-            />
-          ) : null}
+          <Chip
+            title="People Friendly"
+            size="xs"
+            color="#FFD8A9"
+            containerStyle={{ padding: 2 }}
+            titleStyle={{ color: 'black' }}
+          />
         </View>
-        <Text style={{ fontSize: 16, paddingLeft: 5 }}>{selectedUser.bio}</Text>
+        <Text style={{ fontSize: 16, paddingLeft: 5 }}>This is a bio.</Text>
       </View>
       <Button title="Edit Profile" style={{
         backgroundColor: '#fff',
