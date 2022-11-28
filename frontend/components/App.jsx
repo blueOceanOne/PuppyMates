2bc0eeaf449c2374d51f588211d126920e1b2d12
Boyi Qu <boyiqu9596@gmail.com>
2022-11-27 20:51:21 -0800

    uncommented login

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-/* import LogInSignUp from './login/LogInSignUp.jsx'; */
+import LogInSignUp from './login/LogInSignUp.jsx';
 import NavBar from './NavBar.jsx';
 
 const Stack = createNativeStackNavigator();
@@ -11,7 +11,7 @@ export default App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-{/*         <Stack.Screen name="Log In Sign Up" options={{headerShown: false}} component={LogInSignUp} /> */}
+        <Stack.Screen name="Log In Sign Up" options={{headerShown: false}} component={LogInSignUp} />
         <Stack.Screen
           name="App"
           options={{
