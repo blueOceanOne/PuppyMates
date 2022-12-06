00fef8022478f61be2e36c77df4cf6b9c18837c3
avaboonzaayer <sam.boonzaayer@gmail.com>
2022-12-06 16:06:46 -0700

    edited profile styling

--- frontend/components/profile/ViewProfile.jsx
+++ frontend/components/profile/ViewProfile.jsx
@@ -1,76 +1,97 @@
 import React from 'react';
-import { View, Text, Button, Image, StyleSheet, Dimensions } from 'react-native';
+import { View, Text, Button, Image, StyleSheet, Dimensions, TouchableOpacity } from 'react-native';
+import { Card, Header, Chip } from '@rneui/themed';
 
 export default ViewProfile = ({ navigation }) => {
   const dWidth = Dimensions.get('window').width;
   const dHeight = Dimensions.get('window').height;
 
   return (
-    <View style={styles.moreInfo}>
-      <Button title="Log Out" style={{
+    <View style={{
+      backgroundColor: '#fff'
+    }}>
+      <TouchableOpacity style={{
         float: 'right',
         width: 50,
         backgroundColor: '#fff',
         color: '#000'
-      }} />
-      <Image style={{
-        alignSelf: 'center',
-        zIndex: 2,
-        width: 100,
-        height: 100,
-        borderRadius: 50,
-        margin: 10,
-        borderRadius: 20
-      }}
-      source={'https://www.akc.org/wp-content/uploads/2017/11/Longhaired-Dachshund-standing-outdoors.jpg'} />
-      <Text style={{
-        alignSelf: 'center'
-      }}>Lucy</Text>
-      <View>
-        <Text style={{ fontSize: 20, fontWeight: 'bold', paddingLeft: 5 }}>
-          Peoria, AZ
+      }}>
+        <Text>
+          Log Out
         </Text>
-        <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
-          <Chip
-            title="Dachsund"
-            size="xs"
-            color="#FFD8A9"
-            containerStyle={{ padding: 2 }}
-            titleStyle={{ color: 'black' }}
-          />
+      </TouchableOpacity>
+      <View style={styles.moreInfo}>
+        <Image style={{
+          alignSelf: 'center',
+          zIndex: 2,
+          width: 100,
+          height: 200,
+          borderRadius: 50,
+          margin: 10,
+          borderRadius: 100
+        }}
+        source={{uri: 'https://www.akc.org/wp-content/uploads/2017/11/Longhaired-Dachshund-standing-outdoors.jpg'}} />
+        <Text style={{
+          alignSelf: 'center'
+        }}>Lucy</Text>
+        <View>
+          <Text style={{ fontSize: 20, fontWeight: 'bold', paddingLeft: 5 }}>
+            Peoria, AZ
+          </Text>
+          <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
+            <Chip
+              title="Dachsund"
+              size="xs"
+              color="#FFD8A9"
+              containerStyle={{ padding: 2 }}
+              titleStyle={{ color: 'black' }}
+            />
 
-          <Chip
-            title="Small"
-            size="xs"
-            color="#FFD8A9"
-            containerStyle={{ padding: 2 }}
-            titleStyle={{ color: 'black' }}
-          />
+            <Chip
+              title="Small"
+              size="xs"
+              color="#FFD8A9"
+              containerStyle={{ padding: 2 }}
+              titleStyle={{ color: 'black' }}
+            />
 
-          <Chip
-            title="Low Energy"
-            size="xs"
-            color="#FFD8A9"
-            containerStyle={{ padding: 2 }}
-            titleStyle={{ color: 'black' }}
-          />
+            <Chip
+              title="Low Energy"
+              size="xs"
+              color="#FFD8A9"
+              containerStyle={{ padding: 2 }}
+              titleStyle={{ color: 'black' }}
+            />
 
-          <Chip
-            title="People Friendly"
-            size="xs"
-            color="#FFD8A9"
-            containerStyle={{ padding: 2 }}
-            titleStyle={{ color: 'black' }}
-          />
+            <Chip
+              title="People Friendly"
+              size="xs"
+              color="#FFD8A9"
+              containerStyle={{ padding: 2 }}
+              titleStyle={{ color: 'black' }}
+            />
+
+            <Chip
+              title="Dog Friendly"
+              size="xs"
+              color="#FFD8A9"
+              containerStyle={{ padding: 2 }}
+              titleStyle={{ color: 'black' }}
+            />
+          </View>
+          <Text style={{ fontSize: 16, paddingLeft: 5 }}>I love sleeping all day and stealing my brother's bed.</Text>
         </View>
-        <Text style={{ fontSize: 16, paddingLeft: 5 }}>This is a bio.</Text>
+        <TouchableOpacity style={{
+          backgroundColor: '#fff',
+          borderRadius: 10,
+          borderColor: '#000',
+          borderWidth: 4
+        }}>
+          <Text>
+            Edit Profile
+          </Text>
+        </TouchableOpacity>
       </View>
-      <Button title="Edit Profile" style={{
-        backgroundColor: '#fff',
-        borderRadius: 10,
-        borderColor: '#000',
-        borderWidth: 4
-      }} />
     </View>
   )
 }
