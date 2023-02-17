8ac0e7ba5955fc8493536b2a853932eeeeb6809e
Kally Cao <kallycao98@gmail.com>
2023-02-16 16:48:51 -0800

    Modify image thumbnails

--- frontend/components/home/ImageGallery.jsx
+++ frontend/components/home/ImageGallery.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { View, Image, Dimensions, Animated, PanResponder, StyleSheet } from 'react-native';
-import { Text, Card, Button, Icon, Divider } from '@rneui/themed';
+import { Text, Card, Icon, Divider } from '@rneui/themed';
 const { useState, useEffect, useRef } = React;
 
 const ImageGallery = ({ photos, setImgIndex, imgIndex, email, id }) => {
   return (
     <View style={styles.dotContainer}>
       {photos.map((photo, index) => (
-        <Button
-          buttonStyle={imgIndex === index ? styles.button.current : styles.button.hidden}
+        <Icon
+          name={'paw'}
+          type="material-community"
+          iconStyle={imgIndex === index ? styles.icon.current : styles.icon.hidden}
           key={` ${email} - ${id} - ${index}`}
-          style={styles.dots}
           onPress={() => setImgIndex(index)}
         />
       ))}
@@ -21,26 +22,28 @@ export default ImageGallery;
 const styles = StyleSheet.create({
   dotContainer: {
     flexDirection: 'column',
-    height: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // height: '100%',
+    // justifyContent: 'center',
+    // alignItems: 'center',
     top: '50%',
     left: '90%',
     position: 'absolute',
     zIndex: 2,
   },
-  dots: {
-    height: 10,
-    width: 10,
-    padding: 10,
-    zIndex: 2,
-  },
-  button: {
+  icon: {
     current: {
-      backgroundColor: '#F49D1A',
+      // height: 10,
+      // width: 10,
+      // padding: 10,
+      zIndex: 2,
+      color: '#F49D1A',
     },
     hidden: {
-      backgroundColor: '#FFE15D',
+      // height: 10,
+      // width: 10,
+      // padding: 10,
+      zIndex: 2,
+      color: '#FFE15D',
     },
   },
 });
